67aae1e2b3615cc34105bb1d343714fb1a40ac32
jcrowly <john@home-lan.co.uk>
2013-04-18 23:23:50 +0000

    added: unit test for aunpacket.

---
+++ unit-tests/protocol/aunpacketTest.php
@@ -0,0 +1,66 @@
+<?
+
+/*
+ * @group unit-tests
+*/
+
+include_once('include/system.inc.php');
+
+class aunpacketTest extends PHPUnit_Framework_TestCase {
+
+	public function setup()
+	{
+	}
+
+	public function testGetAndSetMethods()
+	{
+		//Set type unicast
+		$sBinaryPacket = pack('C',2);
+
+		//Set port 0x99
+		$sBinaryPacket = $sBinaryPacket . pack('C',0x99);
+		$oPacket = new aunpacket();
+
+		//Set flags 0
+		$sBinaryPacket = $sBinaryPacket . pack('C',0);
+
+		//Set pad 0
+		$sBinaryPacket = $sBinaryPacket . pack('C',0);
+
+		//Sequence 4
+		$sBinaryPacket = $sBinaryPacket . pack('V',4);
+
+		//Data 
+		$sBinaryPacket = $sBinaryPacket . pack('CC',0x90,0);
+
+		$oPacket->decode($sBinaryPacket);
+		
+		//Check header
+		$this->assertEquals($oPacket->getPort(),0x99);
+		$this->assertEquals($oPacket->getPortName(),'FileServerCommand');
+		$this->assertEquals($oPacket->getPacketType(),'Unicast');
+
+		//Check data 
+		$sBinaryData = $oPacket->getData();
+		$aBinaryArray = unpack('C*',$sBinaryData);
+		$this->assertEquals($aBinaryArray[1],0x90);
+		$this->assertEquals($aBinaryArray[2],0);
+
+		//Check we build acks
+		$sAck = $oPacket->buildAck();
+		$aAck = unpack('C*',$sAck);
+		$this->assertEquals($aAck[1],3);
+		
+		
+
+		//Check IP stuff
+		$oPacket->setDestinationIP('192.168.0.1');
+		$oPacket->setSourceIP('192.168.0.2');
+		$this->assertEquals($oPacket->getSourceIP(),'192.168.0.2');
+		$this->assertEquals($oPacket->getDestinationIP(),'192.168.0.1');
+
+	
+	}
+
+}
+?>

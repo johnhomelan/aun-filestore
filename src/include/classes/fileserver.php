165bb2d1f8905e03d87c0efa64ce12242e530b14
jcrowly <john@home-lan.co.uk>
2013-04-18 21:11:14 +0000

    updated: Login now works with username : type logins, a user can log off, tidyed up the code so every method does not need to check if a user is logged in, and started work on make *cat work for clients.

--- src/include/classes/fileserver.php
+++ src/include/classes/fileserver.php
@@ -33,10 +33,24 @@ class fileserver {
 	{
 		$sFunction = $oFsRequest->getFunction();
 		logger::log("FS function ".$sFunction,LOG_DEBUG);
+		//Function where you dont always need to be logged in
 		switch($sFunction){
 			case 'EC_FS_FUNC_CLI':
 				$this->cliDecode($oFsRequest);
+				return;
 				break;
+
+		}
+
+		//Function where the user must be logged in
+		if(!security::isLoggedIn($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation())){
+			$oReply = $oFsRequest->buildReply();
+			$oReply->setError(0xbf,"Who are you?");
+			$this->_addReplyToBuffer($oReply);
+			return;
+		}
+	
+		switch($sFunction){
 			case 'EC_FS_FUNC_LOAD':
 				break;
 			case 'EC_FS_FUNC_SAVE':
@@ -71,12 +85,15 @@ class fileserver {
 			case 'EC_FS_FUNC_GET_DISCS':
 				break;
 			case 'EC_FS_FUNC_GET_INFO':
+				$this->getInfo($oFsRequest);
 				break;
 			case 'EC_FS_FUNC_SET_INFO':
 				break;
 			case 'EC_FS_FUNC_GET_UENV':
+				$this->getUenv($oFsRequest);
 				break;
 			case 'EC_FS_FUNC_LOGOFF':
+				$this->logout($oFsRequest);
 				break;
 			case 'EC_FS_FUNC_GET_USERS_ON':
 				break;
@@ -171,14 +188,20 @@ class fileserver {
 					$sUser = $aOptions[1];
 				}
 				if(array_key_exists(2,$aOptions)){
-					$sPass = $sOptions[2];
+					$sPass = trim($sOptions[2]);
+					if(substr_count($sPass,"\r")>0){
+						list($sPass) = explode("\r",$sPass);
+					}
 				}else{
 					$sPass = "";
 				}
 			}else{
 				$sUser = $aOptions[0];
 				if(array_key_exists(1,$aOptions)){
-					$sPass=$aOptions[1];
+					$sPass = trim($aOptions[1]);
+					if(substr_count($sPass,"\r")>0){
+						list($sPass) = explode("\r",$sPass);
+					}
 				}else{
 					$sPass="";
 				}
@@ -197,12 +220,24 @@ class fileserver {
 
 		//Do login
 		if(security::login($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$sUser,$sPass)){
-			$oUser = securtiy::getUser($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation());
-			$oUrd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oUser->getValue('homedir'));
-			$oCsd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oUser->getValue('homedir'));
-			$oLib = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),config::getValue('library_path'));
+			$oUser = security::getUser($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation());
+			try {
+				$oUrd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oUser->getHomedir());
+				$oCsd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oUser->getHomedir());	
+			}catch(Exception $oException){
+				logger::log("fileserver: Login unable to open homedirectory for user ".$oUser->getUsername(),LOG_INFO);
+				$oUrd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),'');
+				$oCsd = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),'');
+			}
+			try {
+				$oLib = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),config::getValue('library_path'));
+			}catch(Exception $oException){
+				logger::log("fileserver: Login unable to open library dir setting library to $ for user ".$oUser->getUnsername(),LOG_INFO);
+				$oLib = vfs::createFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),'');
+			}
 			$oReply = $oFsRequest->buildReply();
-			$oReply->loginRespone($oUrd->getId(),$oCsd->getId(),$oLib->getId(),$oUser->getValue('opt'));
+			logger::log("fileserver: Login ok urd:".$oUrd->getId()." csd:".$oCsd->getId()." lib:".$oLib->getId(),LOG_DEBUG);
+			$oReply->loginRespone($oUrd->getId(),$oCsd->getId(),$oLib->getId(),$oUser->getBootOpt());
 			$this->_addReplyToBuffer($oReply);
 		}else{
 			$oReply = $oFsRequest->buildReply();
@@ -217,29 +252,104 @@ class fileserver {
 
 	public function logout($oFsRequest)
 	{
-		
+		try{
+			security::logout($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation());
+			$oReply = $oFsRequest->buildReply();	
+			$oReply->DoneOk();
+		}catch(Exception $oException){
+			$oReply = $oFsRequest->buildReply();
+			$oReply->setError(0xbf,"Who are you?");
+		}
+		$this->_addReplyToBuffer($oReply);
+	}
+
+	public function getInfo($oFsRequest)
+	{
+		var_dump($oFsRequest->getByte(1));
+		var_dump($oFsRequest->getString(2));
+		$sDir = $oFsRequest->getString(2);
+		switch($oFsRequest->getByte(1)){
+			case 4:
+				//EC_FS_GET_INFO_ACCESS
+				break;
+			case 5:
+				//EC_FS_GET_INFO_ALL
+				break;
+			case 1:
+				//EC_FS_GET_INFO_CTIME
+				break;
+			case 2:
+				//EC_FS_GET_INFO_META
+				break;
+			case 3:
+				//EC_FS_GET_INFO_SIZE
+				break;
+			case 6:
+				//EC_FS_GET_INFO_DIR
+				try {
+					$oReply = $oFsRequest->buildReply();
+					$oReply->DoneOk();
+					//undef0
+					$oReply->appendByte(0);
+					//zero
+					$oReply->appendByte(0);
+					//ten  need by beeb nfs
+					$oReply->appendByte(10);
+
+					//dir name fixed to 10 bytes right padded with spaces
+					if($sDir==""){
+						//No dir requested so use csd
+						$oFd = vfs::getFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oFsRequest->getCsd());
+						$oReply->appendString(str_pad(substr($oFd->getEconetDirName(),0,10),10,' '));
+					}else{
+						$oReply->appendString(str_pad(substr($sDir,0,10),10,' '));
+					}
+
+					//FS_DIR_ACCESS_PUBLIC
+					$oReply->appendByte(0xff);
+
+					//Cyle  always 0 probably should not be 
+					$oReply->appendByte(0);
+
+					$this->_addReplyToBuffer($oReply);
+
+				}catch(Exception $oException){
+					$oReply = $oFsRequest->buildReply();
+					$oReply->setError(0x8e,"Bad INFO argument");
+					$this->_addReplyToBuffer($oReply);
+				}
+				return;
+				break;
+			case 7:
+				//EC_FS_GET_INFO_UID
+				break;
+			default:
+				$oReply = $oFsRequest->buildReply();
+				$oReply->setError(0x8e,"Bad INFO argument");
+				$this->_addReplyToBuffer($oReply);
+				break;
+		}
+		$oReply = $oFsRequest->buildReply();
+		$oReply->setError(0x8e,"Bad INFO argument");
+		$this->_addReplyToBuffer($oReply);
 	}
 
 	public function eof($oFsRequest)
 	{
-		if(!security::isLoggedIn($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation())){
-			$oReply = $oFsRequest->buildReply();
-			$oReply->setError(0xbf,"Who are you?");
-			
-		}
 		logger::log("Eof Called by ".$oFsRequest->getSourceNetwork().".".$oFsRequest->getSourceStation(),LOG_DEBUG);
 		$oUser = securtiy::getUser($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation());	
 	}
 
 	public function examine($oFsRequest)
 	{
-		if(!security::isLoggedIn($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation())){
-			$oReply = $oFsRequest->buildReply();
-			$oReply->setError(0xbf,"Who are you?");
-			
-		}
-		$iByte1 = $oFsRequest->getByte(1);
-		switch($iByte1){
+		$oReply = $oFsRequest->buildReply();
+		$iArg = $oFsRequest->getByte(1);
+		$iStart = $oFsRequest->getByte(2);
+		$iCount = $oFsRequest->getByte(3);
+		var_dump($iArg);
+		var_dump($iStart);
+		var_dump($iCount);
+		switch($iArg){
 			case 0:
 				//EXAMINE_ALL
 				break;
@@ -251,17 +361,21 @@ class fileserver {
 				break;
 			case 3:
 				//EXAMINE_SHORTTXT
+				//Number of entries 1 Byte
+				$oFd = vfs::getFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oFsRequest->getCsd());
+				$aDirEntries=vfs::getDirectoryListing($oFd);
+				$oReply->appendByte(count($aDirEntries));
+				//Undefined but riscos needs it 
+				$oReply->appendByte(0);
+				
+				//Data
 				break;
 		}
+		$this->_addReplyToBuffer($oReply);
 	}
 
 	public function getArgs($oFsRequest)
 	{
-		if(!security::isLoggedIn($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation())){
-			$oReply = $oFsRequest->buildReply();
-			$oReply->setError(0xbf,"Who are you?");
-			
-		}
 		$iHandle = $oFsRequest->getByte(1);
 		$iArg = $oFsRequest->getByte(2);
 		switch($iArg){
@@ -294,6 +408,35 @@ class fileserver {
 				$oReply->DoneOk();
 				break;
 		}
+		$this->_addReplyToBuffer($oReply);
+	}
+
+	/**
+	 * Gets the current user enviroment
+	 *
+	 * Sends a reply with the name of the disc the csd is on the name of the csd and library
+	*/
+	public function getUenv($oFsRequest)
+	{
+		$oReply = $oFsRequest->buildReply();
+		$oReply->DoneOk();
+		
+		//Discname Max Length <16
+		$oReply->appendByte(16);
+
+		//csd Disc name String 16 bytes
+		$oReply->appendString(str_pad(substr(config::getValue('vfs_disc_name'),0,16),16,' '));
+		
+		//csd Leaf name String 10 bytes
+		$oCsd = vfs::getFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oFsRequest->getCsd());	
+		$oReply->appendString(str_pad(substr($oCsd->getEconetDirName(),0,10),10,' '));
+
+		//lib leaf name String 10 bytes
+		$oLib = vfs::getFsHandle($oFsRequest->getSourceNetwork(),$oFsRequest->getSourceStation(),$oFsRequest->getLib());	
+		var_dump($oLib->getEconetDirName());
+		$oReply->appendString(str_pad(substr($oLib->getEconetDirName(),0,10),10,' '));
+
+		$this->_addReplyToBuffer($oReply);
 	}
 }
 

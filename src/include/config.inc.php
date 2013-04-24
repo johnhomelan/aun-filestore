2b815f5c0de3984b68d7e1f6ed88023c571743cd
jcrowly <john@home-lan.co.uk>
2013-04-24 19:01:53 +0000

    update: Changed default library path.

--- src/include/config.inc.php
+++ src/include/config.inc.php
@@ -12,7 +12,7 @@ safe_define('CONFIG_security_auth_plugins','file');
 safe_define('CONFIG_security_plugin_file_user_file','users.txt');
 safe_define('CONFIG_security_plugin_file_default_crypt','md5');
 
-safe_define('CONFIG_library_path','LIBRARY');
+safe_define('CONFIG_library_path','$.LIBRARY');
 safe_define('CONFIG_vfs_plugins','localfile');
 safe_define('CONFIG_vfs_plugin_localfile_root','/tmp/econetroot/');
 safe_define('CONFIG_vfs_disc_name','VFSROOT');

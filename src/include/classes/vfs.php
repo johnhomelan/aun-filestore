9d458a95a9c85e9d7e703f66e796548e2aa42166
jcrowly <john@home-lan.co.uk>
2013-04-25 22:13:49 +0000

    added: New method createDirectory

--- src/include/classes/vfs.php
+++ src/include/classes/vfs.php
@@ -95,6 +95,31 @@ class vfs {
 		return vfs::$aFileHandleIDs[$oUser->getUserName()];
 	}		
 
+	static public function createDirectory($iNetwork,$iStation,$sEconetPath)
+	{
+		if(!security::isLoggedIn($iNetwork,$iStation)){
+			logger::log("vfs: Un-able to create a handle for a station that is not logged in (Who are you?)",LOG_DEBUG);
+			throw new Exception("vfs: Un-able to create a handle for a station that is not logged in (Who are you?)");
+		}
+		$oUser = security::getUser($iNetwork,$iStation);
+		$sCsd = $oUser->getCsd();
+		$aPlugins = vfs::getVfsPlugins();
+		$oHandle=NULL;
+		foreach($aPlugins as $sPlugin){
+			try {
+				if($sPlugin::createDirectory($oUser,$sCsd,$sEconetPath)){
+					return;
+				}
+			}catch(VfsException $oVfsException){
+				//If it's a hard error abort the operation
+				if($oVfsException->isHard()){
+					throw $oVfsException;
+				}
+			}
+		}
+		throw new Exception("vfs: Unable to create directory (".$sEconetPath.")");
+	}
+
 	/**
 	 * Creates file handle for a given network/station to a given file path
 	 *

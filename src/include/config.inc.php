ecb18b4436bd6941c252c1d5985b4a6d9014078a
jcrowly <john@home-lan.co.uk>
2013-04-30 19:48:18 +0000

    added: Config for file streaming port

--- src/include/config.inc.php
+++ src/include/config.inc.php
@@ -2,6 +2,8 @@
 safe_define('CONFIG_aun_listen_address','0.0.0.0');
 safe_define('CONFIG_aun_listen_port',32768);
 
+safe_define('CONFIG_econet_data_stream_port',0x97);
+
 safe_define('CONFIG_loglevel',LOG_DEBUG);
 safe_define('CONFIG_logstderr',TRUE);
 safe_define('CONFIG_logfile','/tmp/filestore.log');
@@ -11,9 +13,11 @@ safe_define('CONFIG_aun_default_port',32768);
 safe_define('CONFIG_security_auth_plugins','file');
 safe_define('CONFIG_security_plugin_file_user_file','users.txt');
 safe_define('CONFIG_security_plugin_file_default_crypt','md5');
+safe_define('CONFIG_security_default_unix_uid',500);
 
 safe_define('CONFIG_library_path','$.LIBRARY');
 safe_define('CONFIG_vfs_plugins','localfile');
 safe_define('CONFIG_vfs_plugin_localfile_root','/tmp/econetroot/');
 safe_define('CONFIG_vfs_disc_name','VFSROOT');
+safe_define('CONFIG_vfs_home_dir_path','$.home');
 ?>

7df3d3c2071b7138af11bd984b61d5ec3148a3b9
John <john.brown@ask4.com>
2018-07-05 11:59:41 +0100

    added: Packaging stuff needed for docker

---
+++ packaging/docker/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+log(){
+    # Log to stderr
+    logger -s -t entrypoint.sh "$@"
+    }
+
+if [ ! -f /etc/aun-filestored/default.conf ];then 
+	cp /etc/aun-filestored-default-config/default.conf /etc/aun-filestored/default.conf
+fi
+
+if [ ! -f /etc/aun-filestored/users.txt ];then 
+	cp /etc/aun-filestored-default-config/users.txt /etc/aun-filestored/users.txt
+fi
+
+if [ ! -f /etc/aun-filestored/aunmap.txt ];then 
+	cp /etc/aun-filestored-default-config/aunmap.txt /etc/aun-filestored/aunmap.txt
+fi
+
+case "$1" in
+    *)
+	/usr/sbin/filestored -c /etc/aun-filestored
+        ;;
+esac

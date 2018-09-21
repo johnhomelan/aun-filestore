50986040495b15e66f9b54e351598cf596552e16
John <john.brown@ask4.com>
2018-09-21 10:06:26 +0100

    updated: Moved most of the classes into namespaces.

---
+++ src/include/classes/Console/SingleCommandApplication.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HomeLan\FileStore\Console;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+
+class SingleCommandApplication extends Application {
+
+        protected $oCommand;
+
+	protected $sCommandName;
+
+        public function __construct(Command $oCommand)
+        {
+                $this->oCommand = $oCommand;
+                $this->sCommandName = $oCommand->getName();
+                parent::__construct();
+        }
+
+        /**
+         * Gets the default command that should be run
+         *
+         */
+        protected function getDefaultCommands():array
+        {
+                $aCommands = parent::getDefaultCommands();
+                $aCommands[] = $this->oCommand;
+                return $aCommands;
+        }
+
+        /**
+         * Gets the diffinition of the single application
+	 *
+         */
+        public function getDefinition()
+        {
+                $oDefinition = parent::getDefinition();
+                $oDefinition->setArguments();
+
+                return $oDefinition;
+        }
+
+        /**
+         * Gets the name of the command
+         *
+         */
+        protected function getCommandName(InputInterface $input): string
+        {
+                return $this->oCommand->getName();
+        }
+}

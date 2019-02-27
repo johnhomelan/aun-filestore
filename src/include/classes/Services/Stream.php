1289726e16a740561b7dac957d3abab10dd98b33
John Brown <crowly@cray8e.home.lan>
2019-02-27 16:30:55 +0000

    added: Timeout support.

--- src/include/classes/Services/Stream.php
+++ src/include/classes/Services/Stream.php
@@ -12,6 +12,7 @@ class Stream {
 	private $fSucessCallback;
 	private $fFailCallback;
 	private $iTimeout;
+	private $iNoPktTimeout;
 	private $sData;
 
 	public function __construct(int $iPort, int $iBytes,callable $fRecivedPacket, callable $fSucessCallback, callable $fFailCallback,int $iTimeout=60)
@@ -22,20 +23,44 @@ class Stream {
 		$this->fSucessCallback = $fSucessCallback;
 		$this->fFailCallback = $fFailCallback;
 		$this->iTimeout = time() + $iTimeout;
+		$this->iNoPktTimeout = $iTimeout;
 	}
 
+	/** 
+	 * Processes an inbound packet 
+	 *
+	 * If there is still outstanding data this packets data will just get appended to the buffer, and the fRecivedPacket callback is called
+	 * If this packet completes the data stream the fSucessCallback callback is called 
+	 * In the event of a timeout the fFailCallback callback is called
+	*/
 	public function inboundPacket(EconetPacket $oPacket)
 	{
 		$this->sData=$this->sData.$oPacket->getData();
 		if(strlen($this->sData)<$this->iBytes){
 			if(time()>$this->iTimeout){
 				//Timed out 
-				$this->fFailCallback("timeout");
+				($this->fFailCallback)("timeout");
 			}else{
-				$this->fRecivedPacket($this,$oPacket);
+				//Pkt recevied reset the timeout
+				$$this->iTimeout = time() + $this->iNoPktTimeout;
+				($this->fRecivedPacket)($this,$oPacket);
 			}
 		}else{
-			$this->fSucessCallback($this,$this->sData);
+			($this->fSucessCallback)($this,$this->sData);
+		}
+	}
+
+	/**
+	 * Checks if the stream has timed out
+	 *
+	 * If the stream has timed out the fFailCallback callback is called
+	 * If it has not timeout no action is taken
+	*/
+	public function checkTimeout()
+	{
+		if(time()>$this->iTimeout){
+			//Timed out 
+			($this->fFailCallback)("timeout");
 		}
 	}
 }

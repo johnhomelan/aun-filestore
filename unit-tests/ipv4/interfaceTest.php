7688a78c67ffba0659d656a3f2c8f5159412123f
John <john.brown@ask4.com>
2023-07-26 00:49:52 +0100

    added: Unit test for ipv4

---
+++ unit-tests/ipv4/interfaceTest.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * @group unit-tests
+*/
+
+include_once(__DIR__.'/../../src/include/system.inc.php');
+
+use PHPUnit\Framework\TestCase;
+use Monolog\Logger;
+use HomeLan\FileStore\Services\Provider\IPv4\Interfaces;
+use HomeLan\FileStore\Services\Provider\IPv4;
+use HomeLan\FileStore\Services\Provider\IPv4\Exceptions\InterfaceNotFound;
+
+class interfaceTest extends TestCase {
+
+	private Interfaces $oInterfaces;
+
+	protected function setup(): void
+	{
+		$oProvider = $this->createMock(IPv4::class);
+		$sInterfacesFile = "1 24 192.168.1.24 255.255.255.0\n2 24 192.168.2.24 255.255.255.0\n2 26 192.168.2.26 255.255.255.0\n";
+		
+		$this->oInterfaces = new Interfaces($oProvider,$sInterfacesFile);
+	}
+
+	public function testAllValidInterfacesRead()
+	{
+		$this->assertEquals(3,count($this->oInterfaces->dumpInterfaceTable()));
+	}
+
+	public function testInterfaceSelection()
+	{
+		$this->assertEquals(2,$this->oInterfaces->getInterfaceFor('192.168.2.21')['network']);
+		$this->assertEquals(24,$this->oInterfaces->getInterfaceFor('192.168.2.21')['station']);
+	}
+	
+	public function testNoVaildInterface()
+	{
+
+		$this->expectException(InterfaceNotFound::class);
+		$this->oInterfaces->getInterfaceFor('192.168.10.21');
+	}
+}

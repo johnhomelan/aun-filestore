191a4cbf4ed4432f0b8c0a87fab60c56a5e2fec9
John <john.brown@ask4.com>
2023-08-16 21:22:09 +0100

    added: There is now a basic implemention of packet forwarding and handling arp requests and responses.

--- src/include/classes/Services/Provider/IPv4.php
+++ src/include/classes/Services/Provider/IPv4.php
@@ -15,6 +15,14 @@ use HomeLan\FileStore\Services\Provider\IPv4\Admin;
 use HomeLan\FileStore\Aun\Map; 
 use HomeLan\FileStore\Messages\BridgeRequest; 
 use HomeLan\FileStore\Messages\EconetPacket; 
+use HomeLan\FileStore\Messages\ArpRequest; 
+use HomeLan\FileStore\Messages\IPv4Request; 
+use HomeLan\FileStore\Messages\ArpIsAt; 
+use HomeLan\FileStore\Services\Provider\IPv4\Arpcache;
+use HomeLan\FileStore\Services\Provider\IPv4\Interfaces;
+use HomeLan\FileStore\Services\Provider\IPv4\Routes;
+use HomeLan\FileStore\Services\Provider\IPv4\Exceptions\InterfaceNotFound;
+use HomeLan\FileStore\Services\Provider\IPv4\Exceptions\ArpEntryNotFound;
 use config;
 use Exception;
 
@@ -29,6 +37,9 @@ class IPv4 implements ProviderInterface {
 
 	protected $oLogger;
 
+	//Default time to hold IPv4 packets, waiting for an apr response in seconds 
+	const DEFAULT_ARP_WAIT_TIMEOUT = 30;
+
 	/**
 	 * Initializes the service
 	 *
@@ -36,9 +47,12 @@ class IPv4 implements ProviderInterface {
 	public function __construct(\Psr\Log\LoggerInterface $oLogger)
 	{
 		$this->oLogger = $oLogger;
+		$this->oArpTable = new Arpcache($this);
+		$this->oInterfaceTable = new Interfaces($this);
+		$this->oRoutingTable = new Routes($this);
 	}
 
-	protected function _addReplyToBuffer($oReply): void
+	private function addReplyToBuffer($oReply): void
 	{
 		$this->aReplyBuffer[]=$oReply;
 	}
@@ -67,18 +81,42 @@ class IPv4 implements ProviderInterface {
 		return [0xD2];
 	}
 
+
 	/** 
-	 * All inbound bridge messages come in via broadcast 
+	 * Arp who has is the only messsage we deal with via broadcast (the 8byte limit for data means their is not much else we can do with it).
 	 *
 	*/
 	public function broadcastPacketIn(EconetPacket $oPacket): void
 	{
-		$this->processRequest(new BridgeRequest($oPacket,$this->oLogger));
+		//Deal with arp requests
+		if($oPacket->getFlags()==0xA1){ //In EconetA IPv4, the flag is used to indicate the type of packet 0xA1 is arp request
 
+			$oArpReqeust = new ArpRequest($oPacket,$this->oLogger);
+			$this->oArpTable->addEntry($oArpReqeust->getSourceNetwork(),$oArpReqeust->getSourceStation(),$oArpReqeust->getSourceIP());  //Store the requestion stations ip details in the arp cache.
+
+			if($this->oInterfaceTable->isInterfaceIP($oArpReqeust->getRequestedIP())){  //Only reply if the arp request if for an interface IP addr 
+
+				//Get the detials for the relivent interface 
+				$aIf = $this->oInterfaceTable->getInterfaceFor($oArpReqeust->getRequestedIP());
+
+				//Create the arp response (this automatically fills the ip details)
+				$oReply = $oArpReqeust->buildReply();
+
+				//Create the econet packet, and set the hw address for the source to be that of the correct interface 
+				$oReplyPacket= $oReply->buildEconetpacket();
+				$oReplyPacket->setSourceStation($aIf['station']);
+				$oReplyPacket->setSourceNetwork($aIf['network']);
+
+				//Add the packet to the buffer for dispatch  (this is the only example where we don't have to grab the targets hw address from the arpcache)
+				$this->addReplyToBuffer($oReplyPacket);
+			}
+			
+		}
+		
 	}
 
 	/** 
-	 * All inbound bridge messages come in via broadcast, so unicast should ignore them
+	 * Most regular IP packets comes via unicast (given econet limits broadcast to 8 bytes)
 	 *
 	*/
 	public function unicastPacketIn(EconetPacket $oPacket): void
@@ -86,15 +124,72 @@ class IPv4 implements ProviderInterface {
 		switch($oPacket->getFlags()){
 			case 0x1:
 				//Regular IPv4 Frame
+				$oIPv4 = new IPv4Request($oPacket,$this->oLogger);
+
+				//If the IP is for this machine respond
+				if($this->oInterfaceTable->isInterfaceIP($oIPv4->getDstIP())){
+					//@TODO Deal with ICMP echo request 
+					//@TODO Send icmp host unreachable for all other message types 
+					break;
+				};
+
+				//Forward the IP packet
+				try {
+					//First see if we have an interface thats on correct subnet
+					$aInterface = $this->oInterfaceTable->getInterfaceFor($oIPv4->getDstIP());
+					
+					try{
+						$aEconetDst = $this->oArpTable->getNetworkAndStation($oIPv4->getDstIP());
+						$oEconetPacket = $oIPv4->forward($aEconetDst['network'],$aEconetDst['station'],$aInterface['network'],$aInterface['station']);
+						$this->addReplyToBuffer($oEconetPacket);
+					}catch(ArpEntryNotFound $oNotfound){
+						//The address is not in the arp cache send the arp request, and queue the packet after setting 
+						//its source l2 address as the interface that will send the packet once the arp response arrives 
+
+						$oPacket->setSourceNetwork($aInterface['network']);
+						$oPacket->setSourceStation($aInterface['station']);
+						$this->queuePacketWaitingOnArp($oIPv4->getDstIP(),$oPacket);
+
+					}
+				}catch (InterfaceNotFound $oNotfound){
+					//See if we have a route to the subnet 
+					$aRoute = $this->oRoutingTable->getRoute($oIPv4->getDstIP());
+					if(!is_null($aRoute)){
+						try {
+							//Get the interface used to talk to the router
+							$aInterface = $this->oInterfaceTable->getInterfaceFor($aRoute['via']);
+							try {
+								$aEconetDst = $this->oArpTable->getNetworkAndStation($aRoute['via']);
+								$oEconetPacket = $oIPv4->forward($aEconetDst['network'],$aEconetDst['station'],$aInterface['network'],$aInterface['station']);
+								$this->addReplyToBuffer($oEconetPacket);
+							}catch(ArpEntryNotFound $oNotfound){
+								//The l2 address of the router is not in the apr cache, send the  arp request, and queue the packet after setting
+								//its source l2 address as the interface that will send the packet once the arp response arrives
+								//
+								$oPacket->setSourceNetwork($aInterface['network']);
+								$oPacket->setSourceStation($aInterface['station']);
+								this->queuePacketWaitingOnArp($aRoute['via'],$oPacket);
+							}
+						}catch(InterfaceNotFound $oNotfound){
+							//There is no interface with an ip in the same subnet as the router.
+							//Therefor its a invaild route the packet can't be forwarded, so it will be dropped.
+							//
+							//@TODO It should sened ICMP network unreachable
+						}
+					}
+				}
+
 				break;
-			case 0x0a: //ECOTYPE_ARP_SPECIALS
-			case 0x09: //ECOTYPE_ARP_REQUEST
-			case 0x20: //ECOTYPE_ARP_REPLY
-			case 0x21: //ECOTYPE_ARP_REQUEST
-			case 0x22: //ECOTYPE_ARP_REPLY 
-			case 0x23: //ECOTYPE_REVARP_REQUEST
-			case 0x24: //ECOTYPE_REVARP_REPLY
-				//Arp
+			case 0xA2: //ECOTYPE_ARP_REPLY
+				//Arp: We never forward arp packets as they should not leave the layer 2 network they are on, so we only update the arp cache.
+				//
+				//     NB: We don't care if we made an arp request this is a response to, as promiscuous arp is a thing. 
+
+				$oArpIsAt = new ArpIsAt($oPacket,$this->oLogger);
+				$this->oArpTable->addEntry($oArpIsAt->getSourceNetwork(),$oArpIsAt->getSourceStation(),$oArpAsAt->getSourceIP());
+				
+				//Dispatch any IPv4 Packets waiting on the arp response for this IP
+				$this->dequeueWaitingPackets($oArpAsAt->getSourceIP());
 				break;	
 		}
 	}
@@ -102,6 +197,54 @@ class IPv4 implements ProviderInterface {
 
 	public function registerService(ServiceDispatcher $oServiceDispatcher): void
 	{
+		$_this = $this;
+		$this->oServiceDispatcher->addHousingKeepingTask(function() use ($_this){
+			$_this->houseKeeping();
+		});
+	}
+
+	public function houseKeeping():void
+	{
+		$aQueseToBeDropped = [];
+		$iTime = time();
+		//See which queses are over due
+		foreach($this->aPacketQueue as $sIP => $aQueue){
+			if($aQueue['timeout']<$iTime){
+				$aQueseToBeDropped[] = $sIP;
+			}
+		}
+
+		//Delete the overdue queses (assuming there are any)
+		if(count($aQueseToBeDropped)>0){
+			foreach($aQueseToBeDropped as $sIP){
+				unset($this->aPacketQueue[$sIP]);
+			}
+		}
+	}
+
+	public function dequeueWatingPackets(string $sIP):void
+	{
+		if(array_key_exists($sIP,$this->aPacketQueue)){
+			$aAddress = $this->oArpTable->getNetworkAndStation($sIP);
+			foreach($this->aPacketQueue[$sIP]['packets'] as $oPacket){
+				$oPacket->setStation($aAddress['station']);
+				$oPacket->setNetwork($aAddress['network']);
+				$this->addReplyToBuffer($oPacket);
+			}
+			unset($this->aPacketQueue[$sIP]);
+		}
+	}
+
+	public function queuePacketWaitingOnArp(string $sIP, EconetPacket $oPacket):void
+	{
+		if(array_key_exists($sIP,$this->aPacketQueue)){
+			$this->aPacketQueue[$sIP]['packets'][] = $oPacket;
+		}else{
+			$this->aPacketQueue[$sIP] = [];
+			$this->aPacketQueue[$sIP]['packets'] = [$oPacket];
+		}
+			
+		$this->aPacketQueue[$sIP]['timeout'] = time()+ self::DEFAULT_ARP_WAIT_TIMEOUT;
 	}
 
 	/**
@@ -111,83 +254,26 @@ class IPv4 implements ProviderInterface {
 	*/
 	public function getReplies(): array
 	{
-		$aReturn = [];
-		foreach($this->aReplyBuffer as $oReply){
-			$aReturn[] = $oReply->buildEconetpacket();
-		}
-		$this->aReplyBuffer = [];
-		return $aReturn;
+		return $this->aReplyBuffer;
 	}
 
-	/**
-	 * This is the main entry point to this class 
-	 *
-	 * The bridgerequest object contains the request the bridge must process 
-	 * @param bridgerequest $oBridgeRequest
-	*/
-	public function processRequest(BridgeRequest $oBridgeRequest): void
-	{
-		$this->oLogger->debug("Bridge function ".$sFunction);
-		switch($oBridgeRequest->getFunction()){
-			//Bridge to bridge protocol
-			case 'EC_BR_QUERY':
-				break;
-			case 'EC_BR_QUERY2':
-				break;
-			//Station to bridge protocol
-			case 'EC_BR_LOCALNET':
-				$this->queryLocalNet($oBridgeRequest);
-				break;
-			case 'EC_BR_NETKNOWN':
-				$this->queryNetKnown($oBridgeRequest);
-				break;
-			default:
-				throw new Exception("Un-handled bridge request function");
-		}
-	}
-
-
-	/**
-	 * Handle the request to identify the local network
-	 *
-	 * @param bridgerequest $oBridgeRequest
-	*/
-	protected function queryLocalNet(BridgeRequest $oBridgeRequest): void
-	{
-		$oReply = $oBridgeRequest->buildReply();
-		//The first byte of the reply is the local network number	
-		$oReply->appendByte(config::getValue('bridge_local_network_number'));
-		//The second byte is the version number of the bridge firmware 
-		$oReply->appendByte(128);
-		$this->_addReplyToBuffer($oReply);
-	}
-
-	/**
-	 * Handle the request to determine if the birdge knows about a given network
-	 *
-	 * @param bridgerequest $oBridgeRequest
-	*/ 
-	protected function queryNetKnown(BridgeRequest $oBridgeRequest): void
-	{
-		//This first byte after the reply port is the network number the bridge is being queried about
-		$iNetworNumber = $oBridgeRequest->getNetwork();
-		$oReply = $oBridgeRequest->buildReply();
-
-		//Check if the AUN Map knows about the network (as aun is currently our only econet emulation)
-		if(Map::networkKnown($iNetworNumber)){
-			//Network exists (we only reply if we know about a network)
-			$this->_addReplyToBuffer($oReply);
-		}
-
-		//Check the list of networks other bridges can reach
-		if(array_key_exists($iNetworNumber,$this->aRemoteNetworks)){
-			//Network exists (we only reply if we know about a network)
-			$this->_addReplyToBuffer($oReply);
-		}
-	}
-
-	public function getJobs(): array
+	public function getJobs():array
 	{
 		return [];
+	}
+
+	public function getArpEntries(): array
+	{
+		return $this->oArpTable->dumpArpCache();
+	}
+
+	public function getInterfaces(): array
+	{
+		return $this->oInterfaceTable->dumpInterfaceTable();
+	}
+
+	public function getRoutes(): array
+	{
+		return $this->oRoutingTable->dumpRoutingTable();
 	}
 }

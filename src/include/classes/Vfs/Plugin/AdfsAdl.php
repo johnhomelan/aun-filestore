68ab4b86ac142f48b3e5084a3c99d278fdaffcef
John <john.brown@ask4.com>
2026-08-09 21:44:01 +0100

    added missing locking methods

--- src/include/classes/Vfs/Plugin/AdfsAdl.php
+++ src/include/classes/Vfs/Plugin/AdfsAdl.php
@@ -366,6 +366,10 @@ class AdfsAdl implements PluginInterface {
 		throw new VfsException("Read Only FS");
 	}
 
+	public static function fsLock($oUser,$fLocalHandle,bool $bExclusive): void {}
+
+	public static function fsUnlock($oUser,$fLocalHandle): void {}
+
 	public static function fsClose($oUser,$fLocalHandle): void
 	{
 		if(array_key_exists($fLocalHandle,AdfsAdl::$aFileHandles)){
